6ca7f094e453a24a4673435d8dc0cd5a756bc176
Danny <daniel.silva010@mymdc.net>
2024-04-03 06:56:32 -0400

    license.txt and copyrighted some source files, will finish the rest later

--- src/Controllers/ModifyGradesController.java
+++ src/Controllers/ModifyGradesController.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) [2024] [Daniel Silva]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package Controllers;
 
 import java.util.ArrayList;
@@ -8,34 +24,26 @@ import Utils.DataUtil;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
-import javafx.scene.text.Text;
 
 public class ModifyGradesController {
 
     @FXML
-    private Text tCRNCourseName;
+    private TextField tfCRN;
 
     @FXML
-    private Text tGrade;
-
-    @FXML
-    private Text tStudentID;
+    private TextField tfGrade;
 
     @FXML
     private TextField tfStudentID;
 
     @FXML
+    void modify(ActionEvent event) {
+
+    }
+
+    @FXML
     void search(ActionEvent event) {
-        ArrayList<CourseRoster> courseRoster = DataReader.readCourseRoster();
-        if(courseRoster != null) {
-            for(CourseRoster cr : courseRoster) {
-                if(cr.getStudentID().equals(tfStudentID.getText())) {
-                    tStudentID.setText(cr.getStudentID());
-                    tGrade.setText(cr.getGrade());
-                    tCRNCourseName.setText(cr.getCRN() + " " + DataUtil.getCourseName(cr.getCRN()));
-                }
-            }
-        }
+
     }
 
 }
