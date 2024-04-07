68be039be9ead9421a2aba04c54c6609e40ce600
Danny <daniel.silva010@mymdc.net>
2024-04-07 10:25:56 -0400

    calculate GPA

--- src/Controllers/CalculateGPAController.java
+++ src/Controllers/CalculateGPAController.java
@@ -7,10 +7,22 @@ import Utils.DataReader;
 import Utils.DataUtil;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.text.Text;
 
 public class CalculateGPAController {
+
+    private ToggleGroup toggleGroup;
+
+    @FXML
+    private RadioButton rbCRN;
+
+    @FXML
+    private RadioButton rbID;
 
     @FXML
     private Text textGPA;
@@ -19,49 +31,70 @@ public class CalculateGPAController {
     private Text textName;
 
     @FXML
-    private TextField tfID;
+    private TextField tfIDCRN;
 
     @FXML
-    private TextField tfCRN;
+    public void initialize() {
+        toggleGroup = new ToggleGroup();
+        rbCRN.setToggleGroup(toggleGroup);
+        rbID.setToggleGroup(toggleGroup);
+    }
 
     @FXML
     void calculate(ActionEvent event) {
-        String ID = tfID.getText();
-        if(DataUtil.isStudent(ID)) {
-            ArrayList<CourseRoster> roster = DataReader.readCourseRoster();
-            double score = 0;
-            for(int i = roster.size() - 1; i >= 0; i--) {
-                if(roster.get(i).getStudentID().equals(ID)) {
-                    roster.remove(roster.get(i));
+        double score = 0;
+        if(!rbCRN.isSelected() && !rbID.isSelected()) {
+            Alert alert = new Alert(AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText("Error processing");
+            alert.setContentText("Make a selection from the radio buttons");
+        }
+        else if(rbCRN.isSelected() || rbID.isSelected()){
+            if(rbCRN.isSelected() && DataUtil.courseExists(Long.parseLong(tfIDCRN.getText()))) {
+                ArrayList<CourseRoster> roster = DataReader.readCourseRoster();
+                for(int i = roster.size() - 1; i >= 0; i--) {
+                    if(roster.get(i).getCRN() != Long.parseLong(tfIDCRN.getText())) {
+                        roster.remove(roster.get(i));
+                    }
                 }
-            }
 
-            for(CourseRoster oneRecord: roster) {
-                score += getGrade(oneRecord.getGrade());
+                for(CourseRoster oneRecord: roster) {
+                    score += getGrade(oneRecord.getGrade());
+                }
+                score /= roster.size();
+                textName.setText(DataUtil.getCourseName(Long.parseLong(tfIDCRN.getText())));
+                
+                textGPA.setText(String.format("%.2f", score));
             }
-            score /= roster.size();
-            textName.setText(DataUtil.getStudentName(ID));
-            textGPA.setText(String.format("%.2f", score));
+            else if(rbID.isSelected() && DataUtil.studentExists(tfIDCRN.getText())) {
+                ArrayList<CourseRoster> roster = DataReader.readCourseRoster();
+                for(int i = roster.size() - 1; i >= 0; i--) {
+                    if(!roster.get(i).getStudentID().equals(tfIDCRN.getText())) {
+                        roster.remove(roster.get(i));
+                    }
+                }
+                for(CourseRoster oneRecord: roster) {
+                    score += getGrade(oneRecord.getGrade());
+                }
+                score /= roster.size();
+                textName.setText(DataUtil.getStudentName(tfIDCRN.getText()));
+                textGPA.setText(String.format("%.2f", score));
+            }
         }
     }
 
     public static double getGrade(String grade) {
-        if(grade.startsWith("A")) {
+        if (grade.startsWith("A")) {
             return 4.00;
-        }
-        else if(grade.startsWith("B")) {
+        } else if (grade.startsWith("B")) {
             return 3.00;
-        }
-        else if(grade.startsWith("C")) {
+        } else if (grade.startsWith("C")) {
             return 2.00;
-        }
-        else if(grade.startsWith("D")) {
+        } else if (grade.startsWith("D")) {
             return 1.00;
-        }
-        else if(grade.startsWith("F")) {
+        } else if (grade.startsWith("F")) {
             return 0.00;
-        }
-        else {
+        } else {
             return -1;
         }
     }
